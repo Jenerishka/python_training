aa34ad597940d9ea1256952490f831296de0a7b0
e_dumarevskaya <edumarevskaya@gmail.com>
2020-05-04 22:15:43 +0300

    Внесение финальных изменений - объединение двух Application в один

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.group import Group
-from fixture.applicationG import ApplicationG
+from fixture.application import Application
 
 
 @pytest.fixture
 def app(request):
-    fixture = ApplicationG()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
