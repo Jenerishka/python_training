cbba884bb67b93f6cb91fa83486dc9a4168d53ef
e_dumarevskaya <edumarevskaya@gmail.com>
2020-05-05 16:04:31 +0300

    Рефакторинг

--- fixture/application.py
+++ fixture/application.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.contact import ContactHelper
 from fixture.group import GroupHelper
 
 
-class ApplicationG:
+class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.contact = ContactHelper(self)
         self.group = GroupHelper(self)
+
+    def return_to_home_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
 
     def open_home_page(self):
         wd = self.wd
