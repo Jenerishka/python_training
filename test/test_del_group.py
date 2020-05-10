4cdef4df9a565ca222e32cf9b33abf82f106102c
e_dumarevskaya <edumarevskaya@gmail.com>
2020-05-10 20:33:03 +0300

    Added preconditions for group

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+from model.group import Group
 
 
 def test_delete_first_group(app):
     app.session.login(username="admin", password="secret")
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group for test delete group"))
     app.group.delete_first_group()
     app.session.logout()
 
--- test/test_modif_group.py
+++ test/test_modif_group.py
@@ -4,6 +4,8 @@ from model.group import Group
 
 def test_modification_first_group(app):
     app.session.login(username="admin", password="secret")
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group for test modify group"))
     app.group.modif_first_group(Group(name="344fdf", header="dfdf",
                                       footer="sfdfdfew"))
     app.session.logout()
@@ -11,11 +13,15 @@ def test_modification_first_group(app):
 
 def test_modification_first_group_name(app):
     app.session.login(username="admin", password="secret")
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group for test modify group2"))
     app.group.modif_first_group(Group(name="New name"))
     app.session.logout()
 
 
 def test_modification_first_group_header(app):
     app.session.login(username="admin", password="secret")
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group for test modify group3"))
     app.group.modif_first_group(Group(header="New header"))
     app.session.logout()
