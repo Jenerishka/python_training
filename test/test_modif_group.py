7c7ba2d76f98455d015411985a434f79d9d54d27
e_dumarevskaya <edumarevskaya@gmail.com>
2020-05-10 19:10:05 +0300

    Added two tests for modify group Optimization of data input in forms for group

--- test/test_modif_group.py
+++ test/test_modif_group.py
@@ -7,3 +7,15 @@ def test_modification_first_group(app):
     app.group.modif_first_group(Group(name="344fdf", header="dfdf",
                                       footer="sfdfdfew"))
     app.session.logout()
+
+
+def test_modification_first_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modif_first_group(Group(name="New name"))
+    app.session.logout()
+
+
+def test_modification_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modif_first_group(Group(header="New header"))
+    app.session.logout()
