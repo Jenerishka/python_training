73b6e05a6c203d150603d121d8fc0b03be87753c
e_dumarevskaya <edumarevskaya@gmail.com>
2020-05-11 21:14:25 +0300

    Checking the current state of the session

--- fixture/session.py
+++ fixture/session.py
@@ -12,6 +12,20 @@ class SessionHelper:
         except NoSuchElementExeption:
             print("ops")
 
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logget_in():
+            self.logout()
+
+    def is_logget_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logget_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("/html/body/div[1]/div[1]/form/b").text == username
+        # return wd.find_element_by_xpath("//b[contains(.,'(admin)')]").text == username
+
     def login(self, username, password):
         wd = self.app.wd
         self.app.open_home_page()
@@ -21,3 +35,14 @@ class SessionHelper:
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logget_in():
+            if self.is_logget_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+
